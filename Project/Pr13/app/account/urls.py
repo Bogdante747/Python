1a86e910176c1e1b521216c7d8fd38c773219f05
Bogdante747 <152988940+Bogdante747@users.noreply.github.com>
2024-09-25 09:07:20 +0400

    Pr13 add links in base

--- Project/Pr13/app/account/urls.py
+++ Project/Pr13/app/account/urls.py
@@ -4,8 +4,8 @@ from .views import register, login_user, logout_view, profile_view, profile_edit
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register),
-    path('login/', login_user),
+    path('register/', register, name="register"),
+    path('login/', login_user, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/<int:pk>', profile_view, name='profile'),
     path('profile/<int:pk>/edit/', profile_edit.as_view(), name='editProfile'),

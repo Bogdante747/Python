40f53869102311486e5d95c2c45e34a0dff1fc3a
Bogdante747 <152988940+Bogdante747@users.noreply.github.com>
2024-09-26 18:40:30 +0400

    oop2

---
+++ Project/oop2/app/usershema/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsershemaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'usershema'
---
+++ Project/oop2/app/usershema/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from io import TextIOWrapper
+import re
+
+# Create your models here.
+class UserShema:
+    pass
+
+class DataBase:
+    def get_data(self, url):
+        with open(url, 'r', encoding='UTF-8') as f:
+            result = f.readlines()
+            f.close()
+            return result
+        
+    def serializers(self, data:TextIOWrapper):
+        content = []
+        for i in data:
+            schema=dict()
+            line = [i for i in re.split(r'\s', i) if i != '']
+            for index in range(0,len(line)-1, 2):
+                schema[line[index]] = line[index+1]
+            content.append(schema)
+        return content
+    
+    def create(self, data):
+        for i in data:
+            user = UserShema()
+            for key, item in i.items():
+                setattr(user, key, item)

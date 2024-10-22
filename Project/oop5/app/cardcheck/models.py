77ed5a6e1c46331f50b55814b4be2dca2f3b0421
Bogdante747 <152988940+Bogdante747@users.noreply.github.com>
2024-10-22 10:05:40 +0400

    tasks oop5

--- Project/oop5/app/cardcheck/models.py
+++ Project/oop5/app/cardcheck/models.py
@@ -19,3 +19,47 @@ class CardCheck:
         
         first_name, last_name = words
         return all(char in cls.CHARS_FOR_NAME for char in first_name) and all(char in cls.CHARS_FOR_NAME for char in last_name)
+    
+class TemperatureConverter:
+    celsius = 0
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        fahrenheit = 1.8 * celsius + 32
+        return fahrenheit
+    
+    @classmethod
+    def from_kelvin(cls, kelvin):
+        celsius = kelvin - 273
+        cls.celsius = celsius
+        return celsius
+    
+
+class Employee:
+
+    @staticmethod
+    def is_valid_age(age):
+        if age < 18:
+            return "Молодой"
+        elif age > 65:
+            return "Старый"
+        else:
+            return age
+        
+    @classmethod
+    def from_string(cls, name, age, post):
+        if 65 >= age >= 18:
+            cls.name = name
+            cls.age = age
+            cls.post = post
+            return "Четко"
+        else:
+            raise ValueError("Не повезло, не фортануло")
+        
+    @classmethod
+    def get_details(cls):
+        return f"Имя: {cls.name}, Возраст: {cls.age}, Должность: {cls.post}"
+    
+e1 = Employee()
+
+print(e1.from_string("asd", 20, "ad"))
+print(e1.get_details())

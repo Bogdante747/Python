e789158832f2f8a61401c4cf7e6970e9859a04f1
Bogdante747 <152988940+Bogdante747@users.noreply.github.com>
2024-10-22 10:21:08 +0400

    tasks oop2

--- Project/oop2/app/usershema/models.py
+++ Project/oop2/app/usershema/models.py
@@ -28,3 +28,21 @@ class DataBase:
             user = UserShema()
             for key, item in i.items():
                 setattr(user, key, item)
+
+
+class Translator:
+    def __init__(self): 
+        self.words = {} 
+
+    def add(self, eng, rus): 
+        if eng not in self.words.keys(): 
+            self.words[eng] = [] 
+        self.words[eng].append(rus) 
+
+    def remove(self, eng): 
+        self.words.pop(eng) 
+
+    def translate(self, eng): 
+        for i in self.words[eng]: 
+            print(i, end=' ') 
+        print() 
